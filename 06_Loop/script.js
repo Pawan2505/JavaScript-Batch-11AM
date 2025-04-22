5961dbd9fb1f2ac538255b86b82bbc8b07f71bea
pawan2505 <pawanaktu@gmail.com>
2025-04-22 11:08:08 +0530

    loop

---
+++ 06_Loop/script.js
@@ -0,0 +1,45 @@
+// console.log("1")
+// console.log("2");
+// console.log("3");
+// console.log("4");
+
+// for loop -> range -> start & end
+// while loop ->  condition -> true
+// do while loop -> same as while loop only diffrence -> one time -> loop -> chalta hai-> second time jab loop chalta hai -> condition check hota hai
+
+// Sum of numbers in a given range
+
+// 10 to 50
+
+// let num = 10;
+// let sum = 0;
+
+// for (let i = num; i <= 50; i++){
+//     // console.log(i)
+//     sum += i;
+// }
+
+// console.log(sum)
+
+// let num = 10;
+// let sum = 0;
+
+// while (num <= 50) {
+//   sum += num;
+//   num++;
+// }
+
+// console.log(sum);
+
+
+
+// let num = 10;
+// let sum = 0;
+
+// do {
+//     sum += num;
+//     num++;
+// }while(num<=50)
+
+
+// console.log(sum)
