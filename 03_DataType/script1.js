40e94ee346d01b65988fa59ac11f41cf6711d23d
pawan2505 <pawanaktu@gmail.com>
2025-04-25 12:10:30 +0530

    variable & datatype

---
+++ 03_DataType/script1.js
@@ -0,0 +1,144 @@
+// https://www.w3schools.com/js/js_reserved.asp
+
+//  x = 10; // not recommended
+// console.log(x)
+
+// var username = "Pawan"
+// console.log(username);
+
+// var 12num; // not ok
+
+// var num12 = 123;  // ok
+// console.log(num12)
+
+// let first name; // not ok
+
+// let first_name; //ok
+
+// let if; //not ok
+
+// let @num; // not ok
+
+// only two special character we can use with variable name -> $,_
+
+// const $num = 10;
+// console.log($num)
+
+// const num$ = 10;
+// console.log(num$);
+
+// const _num$ = 10;
+// console.log(_num$);
+
+// let num = 10;
+// let Num = 20;
+
+// console.log(Num);
+// console.log(num)
+
+// Declaration keyword
+
+// var num = 10;
+
+// var num = 20;
+
+// var username = "vraj";
+// username = "Fatima";
+
+// console.log(username)
+
+// var keyword variable not scoped (block scope) variable
+// {
+//     var num = 20; // local variable
+//     console.log(num);
+// }
+
+// console.log(num)
+
+//  var num = 20;  // global variable
+// {
+//   console.log(num);
+// }
+
+// console.log(num);
+
+// let
+
+// let num = 10;
+// console.log(num);
+// num = 50;
+// console.log(num)
+
+// let num = 10;
+// console.log(num);
+// let num = 50;
+// console.log(num);
+
+
+// let is block scope variable
+
+
+// {
+//     let num = 10;
+//     console.log(num)
+// }
+
+
+// {
+//   let num = 10;
+  
+// }
+
+// console.log(num);
+
+// pref : local variable > gloable variable
+
+// let num = 10;
+// {
+//     let num = 100;
+//     console.log(num); // 100
+// }
+
+// console.log(num); //10
+
+// let num = 10; // global variable
+// {
+
+//     console.log(num);
+// }
+
+// let num;
+// num = 900;
+
+// console.log(num)
+
+// const
+
+// const num;
+// num = 10;  // not ok
+// console.log(num)
+
+// const num = 10;
+// console.log(num)
+
+// {
+//     const num = 70;
+// }
+
+// console.log(num)  // not accessable
+
+
+// {
+//   const num = 70;
+//   console.log(num);
+// }
+
+
+//  const num = 70;
+// {
+
+//   console.log(num);
+// }
+
+
+
