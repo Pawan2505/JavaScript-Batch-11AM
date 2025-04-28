0da5a3173e4ae52774f1f9cc8e89880d9d1bb729
pawan2505 <pawanaktu@gmail.com>
2025-04-28 14:25:43 +0530

    code

---
+++ 03_DataType/p.js
@@ -0,0 +1,94 @@
+// number
+
+// let num = 10;
+// console.log(num)
+// console.log(typeof num)
+
+// let num = 10.5;
+// console.log(num);
+// console.log(typeof num);
+
+// string
+
+// const username = "Pawan Maurya";
+// console.log(username)
+// console.log(typeof(username))
+
+
+// const username = 'Pawan Maurya';
+// console.log(username)
+// console.log(typeof(username))
+
+
+// const username = 'P';
+// console.log(username);
+// console.log(typeof username);
+
+// boolean
+
+// let isPresent = false;
+// console.log(isPresent)
+
+// console.log(typeof isPresent)
+
+// undefined
+
+// let sum;
+
+// console.log(sum)
+
+// console.log(typeof sum)
+
+
+// bigint
+
+// let num = 121234325n;
+// console.log(num)
+// console.log(typeof num);
+
+// object
+
+// let num = null;
+
+// console.log(num)
+// console.log(typeof num)
+
+// let arr = [12, "Pawan", true, undefined];
+
+// console.log(arr);
+
+// console.log(typeof arr);
+
+
+// const add = () => {
+//     console.log("Array Function!")
+// }
+
+// add();
+
+// console.log(typeof add)
+
+// function add() {
+//     console.log("Normal Function!");
+// }
+
+// add();
+
+// console.log(typeof add())
+
+
+// let obj = {
+//     id: 1,
+//     name:"Pawan"
+// }
+
+// console.log(obj)
+// console.log(typeof obj);
+
+
+// let num = Symbol(10);
+// let num1 = Symbol(10);
+// console.log(typeof num)
+
+// console.log(num)
+// console.log(num==num1)
